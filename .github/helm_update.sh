1c36640a59c460f0a90ee250bd367a40356bb6a9
Ysmaël Fortier <32311542+ysma500@users.noreply.github.com>
2021-11-05 17:32:25 -0400

    Edit for cert-manager in helm update

--- .github/helm_update.sh
+++ .github/helm_update.sh
@@ -15,8 +15,8 @@ case $1 in
     mongodb)
         helm upgrade mongodb --install --version 10.26.0 bitnami/mongodb -f chart/values.yaml
     ;;
-    pebble)
-        helm upgrade pebble --install --version 0.2.2 jupyterhub/pebble -f chart/values.yaml
+    cert-manager)
+        helm upgrade cert-manager --install --version 1.6.0 jetstack/cert-manager -f chart/values.yaml
     ;;
     traefik)
         helm upgrade traefik --install --version 10.3.4 traefik/traefik -f values.yaml
